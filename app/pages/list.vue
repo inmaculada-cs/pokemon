1ca94d26d8eba7904e2a2fa6af056a6d5e4fba5b
Francisco Mejía <fmejiaantezano@gmail.com>
2024-11-22 09:28:37 -0500

    link

--- app/pages/list.vue
+++ app/pages/list.vue
@@ -3,16 +3,19 @@ const { data } = useFetch("/api/list-pokemon");
 </script>
 
 <template>
-  <div class="container py-8 px-2 sm:px-6">
-    <div class="grid grid-cols-2 gap-4">
-      <template v-for="pokemon in data" :key="pokemon.name">
-        <div class="p-2 border rounded">
-          <img :src="pokemon.imageUrl" alt="" />
-          <p>Name: {{ pokemon.name }}</p>
-          <p>Type: {{ pokemon.type }}</p>
-          <p>Created: {{ new Date(pokemon.timestamp) }}</p>
-        </div>
-      </template>
+  <div>
+    <NuxtLink to="/">Home</NuxtLink>
+    <div class="container py-8 px-2 sm:px-6">
+      <div class="grid grid-cols-2 gap-4">
+        <template v-for="pokemon in data" :key="pokemon.name">
+          <div class="p-2 border rounded">
+            <img :src="pokemon.imageUrl" alt="" />
+            <p>Name: {{ pokemon.name }}</p>
+            <p>Type: {{ pokemon.type }}</p>
+            <p>Created: {{ new Date(pokemon.timestamp) }}</p>
+          </div>
+        </template>
+      </div>
     </div>
   </div>
 </template>
